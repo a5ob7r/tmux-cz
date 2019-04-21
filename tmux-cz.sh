d671cf41a61059ee6a20f5049eca666164c3f3b7
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2019-04-21 16:15:00 +0900

    Add prefix to color constants name
    Prevent them and shell environment variables to conflicting. For
    example, color variables may be used to define colors of ls command per
    file kinds.

--- tmux-cz.sh
+++ tmux-cz.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # {{{ color
-readonly BLACK="colour0"
-readonly WHITE="colour15"
-readonly GREEN="colour29"
-readonly YELLOW_GREEN="colour108"
-readonly RED="colour160"
-readonly ORANGE="colour202"
-readonly DARK_ORANGE="colour208"
-readonly NAVAJO_WHITE="colour223"
-readonly LIGHT_BLACK="colour235"
-readonly DARK_GRAY="colour237"
-readonly LIGHT_GRAY="colour243"
+readonly TMUX_CZ_BLACK="colour0"
+readonly TMUX_CZ_WHITE="colour15"
+readonly TMUX_CZ_GREEN="colour29"
+readonly TMUX_CZ_YELLOW_GREEN="colour108"
+readonly TMUX_CZ_RED="colour160"
+readonly TMUX_CZ_ORANGE="colour202"
+readonly TMUX_CZ_DARK_ORANGE="colour208"
+readonly TMUX_CZ_NAVAJO_WHITE="colour223"
+readonly TMUX_CZ_LIGHT_BLACK="colour235"
+readonly TMUX_CZ_DARK_GRAY="colour237"
+readonly TMUX_CZ_LIGHT_GRAY="colour243"
 # }}}
 
 # {{{ function
@@ -32,15 +32,15 @@ tmux set -g status-interval 1
 tmux set -g status-justify left
 
 # base color
-tmux set -g status-style bg=$LIGHT_BLACK,fg=$DARK_ORANGE
+tmux set -g status-style bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE
 # }}}
 
 # {{{ window status
-window_index="#[fg=$WHITE]#[bg=$LIGHT_GRAY] #{window_index}#{window_flags} "
-window_name="#[bg=$DARK_GRAY] #{window_name} "
+window_index="#[fg=$TMUX_CZ_WHITE]#[bg=$TMUX_CZ_LIGHT_GRAY] #{window_index}#{window_flags} "
+window_name="#[bg=$TMUX_CZ_DARK_GRAY] #{window_name} "
 
-cur_window_index="#[bg=$YELLOW_GREEN]#[fg=$BLACK] #{window_index}#{window_flags} "
-cur_window_name="#[bg=$NAVAJO_WHITE] #{window_name} "
+cur_window_index="#[bg=$TMUX_CZ_YELLOW_GREEN]#[fg=$TMUX_CZ_BLACK] #{window_index}#{window_flags} "
+cur_window_name="#[bg=$TMUX_CZ_NAVAJO_WHITE] #{window_name} "
 
 tmux set -g window-status-format "${window_index}${window_name}"
 tmux set -g window-status-current-format "${cur_window_index}${cur_window_name}"
@@ -49,19 +49,19 @@ tmux set -g window-status-current-format "${cur_window_index}${cur_window_name}"
 # {{{ left and right status
 # color block
 default_block=$(block)$(block)
-red_block=$(block $RED)
-orange_block=$(block $ORANGE)
-green_block=$(block $GREEN)
-yellow_green_block=$(block $YELLOW_GREEN)
-light_gray_block=$(block $LIGHT_GRAY)
-dark_gray_block=$(block $DARK_GRAY)
+red_block=$(block $TMUX_CZ_RED)
+orange_block=$(block $TMUX_CZ_ORANGE)
+green_block=$(block $TMUX_CZ_GREEN)
+yellow_green_block=$(block $TMUX_CZ_YELLOW_GREEN)
+light_gray_block=$(block $TMUX_CZ_LIGHT_GRAY)
+dark_gray_block=$(block $TMUX_CZ_DARK_GRAY)
 
 color_blocks="${red_block}${orange_block}${green_block}${yellow_green_block}${light_gray_block}${dark_gray_block}"
 color_blocks_reverse="${dark_gray_block}${light_gray_block}${yellow_green_block}${green_block}${orange_block}${red_block}"
 
-host_name="#{?client_prefix,#[fg=$DARK_GRAY]#[bg=$DARK_ORANGE]#[bold],#[bg=$DARK_GRAY]} #H [#S] "
+host_name="#{?client_prefix,#[fg=$TMUX_CZ_DARK_GRAY]#[bg=$TMUX_CZ_DARK_ORANGE]#[bold],#[bg=$TMUX_CZ_DARK_GRAY]} #H [#S] "
 battery_status="${dark_gray_block}#{battery_status_fg}#{battery_icon} #{battery_percentage}${dark_gray_block}"
-clock="#[bg=$DARK_GRAY] %H:%M %Y-%m-%d(%a) "
+clock="#[bg=$TMUX_CZ_DARK_GRAY] %H:%M %Y-%m-%d(%a) "
 status_right="${clock}${default_block}${color_blocks_reverse}"
 
 tmux set -g status-left-length 90
@@ -76,19 +76,19 @@ fi
 # }}}
 
 # {{{ pane
-tmux set -g pane-border-style fg=$DARK_GRAY
-tmux set -g pane-active-border-style fg=$DARK_ORANGE
+tmux set -g pane-border-style fg=$TMUX_CZ_DARK_GRAY
+tmux set -g pane-active-border-style fg=$TMUX_CZ_DARK_ORANGE
 # }}}
 
 # {{{ mode
-tmux set -g mode-style bg=$DARK_ORANGE,fg=$LIGHT_BLACK
+tmux set -g mode-style bg=$TMUX_CZ_DARK_ORANGE,fg=$TMUX_CZ_LIGHT_BLACK
 # }}}
 
 # {{{ message
-tmux set -g message-style bg=$LIGHT_BLACK,fg=$DARK_ORANGE
+tmux set -g message-style bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE
 # }}}
 
 # {{{ display pane number
-tmux set -g display-panes-colour $YELLOW_GREEN
-tmux set -g display-panes-active-colour $DARK_ORANGE
+tmux set -g display-panes-colour $TMUX_CZ_YELLOW_GREEN
+tmux set -g display-panes-active-colour $TMUX_CZ_DARK_ORANGE
 # }}}
