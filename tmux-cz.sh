7b203a0d68fa6b9a06d1ef90e7d34d983ce75d48
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-11-09 22:50:53 +0900

    Add double quotations to variable expansions

--- tmux-cz.sh
+++ tmux-cz.sh
@@ -20,37 +20,37 @@ readonly TMUX_CZ_LIGHT_GRAY="colour243"
 # }}}
 
 # Left Decoration
-readonly TMUX_CZ_ORANGE_GREEN_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_ORANGE} ${TMUX_CZ_GREEN})"
-readonly TMUX_CZ_GREEN_YELLOW_GREEN_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_GREEN} ${TMUX_CZ_YELLOW_GREEN})"
-readonly TMUX_CZ_YELLOW_GREEN_LIGHT_GRAY_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_YELLOW_GREEN} ${TMUX_CZ_LIGHT_GRAY})"
-readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_LIGHT_GRAY} ${TMUX_CZ_DARK_GRAY})"
-readonly TMUX_CZ_LIGHT_GRAY_DARK_ORANGE_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_LIGHT_GRAY} ${TMUX_CZ_DARK_ORANGE})"
-readonly TMUX_CZ_LIGHT_BLACK_LIGHT_GRAY_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_LIGHT_BLACK} ${TMUX_CZ_LIGHT_GRAY})"
-readonly TMUX_CZ_LIGHT_BLACK_YELLOW_GREEN_LEFT_DECORATION_INFLATION="$(left_decoration_inflation ${TMUX_CZ_LIGHT_BLACK} ${TMUX_CZ_YELLOW_GREEN})"
+readonly TMUX_CZ_ORANGE_GREEN_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_ORANGE}" "${TMUX_CZ_GREEN}")
+readonly TMUX_CZ_GREEN_YELLOW_GREEN_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_GREEN}" "${TMUX_CZ_YELLOW_GREEN}")
+readonly TMUX_CZ_YELLOW_GREEN_LIGHT_GRAY_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_YELLOW_GREEN}" "${TMUX_CZ_LIGHT_GRAY}")
+readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_LIGHT_GRAY}" "${TMUX_CZ_DARK_GRAY}")
+readonly TMUX_CZ_LIGHT_GRAY_DARK_ORANGE_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_LIGHT_GRAY}" "${TMUX_CZ_DARK_ORANGE}")
+readonly TMUX_CZ_LIGHT_BLACK_LIGHT_GRAY_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_LIGHT_BLACK}" "${TMUX_CZ_LIGHT_GRAY}")
+readonly TMUX_CZ_LIGHT_BLACK_YELLOW_GREEN_LEFT_DECORATION_INFLATION=$(left_decoration_inflation "${TMUX_CZ_LIGHT_BLACK}" "${TMUX_CZ_YELLOW_GREEN}")
 
 # Right Decoration
-readonly TMUX_CZ_ORANGE_GREEN_RIGHT_DECORATION_INFLATION="$(right_decoration_inflation ${TMUX_CZ_ORANGE} ${TMUX_CZ_GREEN})"
-readonly TMUX_CZ_GREEN_YELLOW_GREEN_RIGHT_DECORATION_INFLATION="$(right_decoration_inflation ${TMUX_CZ_GREEN} ${TMUX_CZ_YELLOW_GREEN})"
-readonly TMUX_CZ_YELLOW_GREEN_LIGHT_GRAY_RIGHT_DECORATION_INFLATION="$(right_decoration_inflation ${TMUX_CZ_YELLOW_GREEN} ${TMUX_CZ_LIGHT_GRAY})"
-readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_RIGHT_DECORATION_INFLATION="$(right_decoration_inflation ${TMUX_CZ_LIGHT_GRAY} ${TMUX_CZ_DARK_GRAY})"
+readonly TMUX_CZ_ORANGE_GREEN_RIGHT_DECORATION_INFLATION=$(right_decoration_inflation "${TMUX_CZ_ORANGE}" "${TMUX_CZ_GREEN}")
+readonly TMUX_CZ_GREEN_YELLOW_GREEN_RIGHT_DECORATION_INFLATION=$(right_decoration_inflation "${TMUX_CZ_GREEN}" "${TMUX_CZ_YELLOW_GREEN}")
+readonly TMUX_CZ_YELLOW_GREEN_LIGHT_GRAY_RIGHT_DECORATION_INFLATION=$(right_decoration_inflation "${TMUX_CZ_YELLOW_GREEN}" "${TMUX_CZ_LIGHT_GRAY}")
+readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_RIGHT_DECORATION_INFLATION=$(right_decoration_inflation "${TMUX_CZ_LIGHT_GRAY}" "${TMUX_CZ_DARK_GRAY}")
 
 # Left Separator
-readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_LEFT_SEPARATOR="$(left_separator ${TMUX_CZ_LIGHT_GRAY} ${TMUX_CZ_DARK_GRAY})"
-readonly TMUX_CZ_DARK_GRAY_LIGHT_BLACK_LEFT_SEPARATOR="$(left_separator ${TMUX_CZ_DARK_GRAY} ${TMUX_CZ_LIGHT_BLACK})"
-readonly TMUX_CZ_YELLOW_GREEN_NAVAJO_WHITE_LEFT_SEPARATOR="$(left_separator ${TMUX_CZ_YELLOW_GREEN} ${TMUX_CZ_NAVAJO_WHITE})"
-readonly TMUX_CZ_NAVAJO_WHITE_LIGHT_BLACK_LEFT_SEPARATOR="$(left_separator ${TMUX_CZ_NAVAJO_WHITE} ${TMUX_CZ_LIGHT_BLACK})"
-readonly TMUX_CZ_DARK_ORANGE_LIGHT_BLACK_LEFT_SEPARATOR="$(left_separator ${TMUX_CZ_DARK_ORANGE} ${TMUX_CZ_LIGHT_BLACK})"
+readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_LEFT_SEPARATOR=$(left_separator "${TMUX_CZ_LIGHT_GRAY}" "${TMUX_CZ_DARK_GRAY}")
+readonly TMUX_CZ_DARK_GRAY_LIGHT_BLACK_LEFT_SEPARATOR=$(left_separator "${TMUX_CZ_DARK_GRAY}" "${TMUX_CZ_LIGHT_BLACK}")
+readonly TMUX_CZ_YELLOW_GREEN_NAVAJO_WHITE_LEFT_SEPARATOR=$(left_separator "${TMUX_CZ_YELLOW_GREEN}" "${TMUX_CZ_NAVAJO_WHITE}")
+readonly TMUX_CZ_NAVAJO_WHITE_LIGHT_BLACK_LEFT_SEPARATOR=$(left_separator "${TMUX_CZ_NAVAJO_WHITE}" "${TMUX_CZ_LIGHT_BLACK}")
+readonly TMUX_CZ_DARK_ORANGE_LIGHT_BLACK_LEFT_SEPARATOR=$(left_separator "${TMUX_CZ_DARK_ORANGE}" "${TMUX_CZ_LIGHT_BLACK}")
 
 # Right Separator
-readonly TMUX_CZ_DARK_GRAY_LIGHT_BLACK_RIGHT_SEPARATOR="$(right_separator ${TMUX_CZ_DARK_GRAY} ${TMUX_CZ_LIGHT_BLACK})"
+readonly TMUX_CZ_DARK_GRAY_LIGHT_BLACK_RIGHT_SEPARATOR=$(right_separator "${TMUX_CZ_DARK_GRAY}" "${TMUX_CZ_LIGHT_BLACK}")
 
 # Right Subseparator
-readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_RIGHT_SUBSEPARATOR="$(right_subseparator ${TMUX_CZ_LIGHT_GRAY} ${TMUX_CZ_DARK_GRAY})"
+readonly TMUX_CZ_LIGHT_GRAY_DARK_GRAY_RIGHT_SUBSEPARATOR=$(right_subseparator "${TMUX_CZ_LIGHT_GRAY}" "${TMUX_CZ_DARK_GRAY}")
 # }}}
 
 # {{{ status
 # base color
-tmux set -g status-style bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE
+tmux set -g status-style "bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE"
 # }}}
 
 # {{{ window status
@@ -99,19 +99,19 @@ tmux set -g status-right "${STATUS_RIGHT}"
 # }}}
 
 # {{{ pane
-tmux set -g pane-border-style fg=$TMUX_CZ_DARK_GRAY
-tmux set -g pane-active-border-style fg=$TMUX_CZ_DARK_ORANGE
+tmux set -g pane-border-style "fg=$TMUX_CZ_DARK_GRAY"
+tmux set -g pane-active-border-style "fg=$TMUX_CZ_DARK_ORANGE"
 # }}}
 
 # {{{ mode
-tmux set -g mode-style bg=$TMUX_CZ_DARK_ORANGE,fg=$TMUX_CZ_LIGHT_BLACK
+tmux set -g mode-style "bg=$TMUX_CZ_DARK_ORANGE,fg=$TMUX_CZ_LIGHT_BLACK"
 # }}}
 
 # {{{ message
-tmux set -g message-style bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE
+tmux set -g message-style "bg=$TMUX_CZ_LIGHT_BLACK,fg=$TMUX_CZ_DARK_ORANGE"
 # }}}
 
 # {{{ display pane number
-tmux set -g display-panes-colour $TMUX_CZ_YELLOW_GREEN
-tmux set -g display-panes-active-colour $TMUX_CZ_DARK_ORANGE
+tmux set -g display-panes-colour "$TMUX_CZ_YELLOW_GREEN"
+tmux set -g display-panes-active-colour "$TMUX_CZ_DARK_ORANGE"
 # }}}
